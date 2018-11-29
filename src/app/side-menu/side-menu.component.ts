15426c5abc825c9b5e384c3dc82dc86e47b06086
webtoor <fityanalimunshi@gmail.com>
2018-11-29 14:21:41 +0700

    request logout to backend

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-side-menu',
@@ -10,19 +11,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent {
-
+  data_admin:any;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, public router : Router) {}
+  constructor(public authService: AuthService, private breakpointObserver: BreakpointObserver, public router : Router) {
+    const data  = localStorage.getItem('adminData');
+    this.data_admin = JSON.parse(data);
+  }
   
   ngOnInit(){
     //console.log(localStorage.getItem('adminData'));
   }
   logoutUser(){
-    localStorage.removeItem('adminData');
-    this.router.navigate(['/login']);
+    this.authService.getData('api/logout', this.data_admin['access_token']).subscribe(record => {
+      console.log(record)
+      localStorage.removeItem('adminData');
+      this.router.navigate(['/login']);
+    });
+
   }
 }
