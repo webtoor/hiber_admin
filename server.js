1bc3503df61e6c736cc0da723854370645b65617
webtoor <fityanalimunshi@gmail.com>
2018-12-08 16:52:23 +0700

    new heriku

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const path = require('path');
+//Install express server    
 const express = require('express');
-const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + './dist'));
+const path = require('path');   
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+const app = express();   
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
