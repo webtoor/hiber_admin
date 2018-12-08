7281cdb2ab80fbfb04fe1ab316061691b3ffbf1c
webtoor <fityanalimunshi@gmail.com>
2018-12-08 17:04:41 +0700

    news

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-//Install express server    
 const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
 
-const path = require('path');   
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-const app = express();   
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/src'));
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/src/index.html'));   
-});  
+// PathLocationStrategy
 
-// Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 8080);
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
