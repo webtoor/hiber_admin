154c2916c5d0592a93441d68eb7cdc00aca4be1d
webtoor <fityanalimunshi@gmail.com>
2018-12-08 16:22:51 +0700

    server.js 2

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/hiber-admin'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/hiber-admin/index.html'));
 });
 
 // default Heroku port
